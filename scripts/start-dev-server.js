7865ebcd5e06c29f69cf4b82e21ba1cda27bc782
Jonathan Lafleur <me@jonathanlafleur.ca>
2018-04-20 15:39:16 -0300

    Auto stash before revert of "Downgrade some packages, add/remove other packages."

---
+++ scripts/start-dev-server.js
@@ -0,0 +1,52 @@
+/**
+ * Webpack Dev Server
+ * This file is used to run our local environment
+ */
+
+const webpack = require('webpack');
+const express = require('express');
+const WebpackDevMiddleware = require('webpack-dev-middleware');
+const WebpackHotMiddleware = require('webpack-hot-middleware');
+const fallback = require('express-history-api-fallback');
+const path = require('path');
+const webpackConfig = require('./webpack.config');
+
+/**
+ * Always dev environment when running webpack dev server
+ * There are other ways to do this, so feel free to do
+ * whatever you find suites your taste
+ */
+
+const port = process.env.PORT || 3000;
+
+const env = {
+		dev: process.env.NODE_ENV === 'development',
+		port
+};
+
+const devServerConfig = {
+		hot: true,
+		inline: true,
+		https: false,
+		lazy: false,
+		contentBase: path.join(__dirname, '../src/frontend'),
+		historyApiFallback: { disableDotRule: true }, // Need historyApiFallback to be able to refresh on dynamic route
+		stats: { colors: true } // Pretty colors in console
+};
+
+try {
+		const app = express();
+		const compiler = webpack(webpackConfig(env));
+		const devMiddleware = WebpackDevMiddleware(compiler, devServerConfig);
+		const hotMiddleware = WebpackHotMiddleware(compiler);
+		app.use(devMiddleware);
+		app.use(hotMiddleware);
+		app.listen(port, 'localhost', err => {
+				if (err) {
+						console.error(err);
+				}
+				console.log(`Server listening to port ${port}`);
+		});
+} catch (e) {
+		console.error(e);
+}
---
+++ scripts/webpack.config.js
@@ -0,0 +1,108 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = (env) => {
+		const ifProd = plugin => env.prod ? plugin : undefined;
+		const ifDev = plugin => env.dev ? plugin : undefined;
+		const removeEmpty = array => array.filter(p => !!p);
+		const NODE_ENV = env.prod ? 'production' : 'development';
+		return {
+				devtool: ifDev('source-map'),
+				entry: {
+						app: removeEmpty([
+								ifDev('react-hot-loader/patch'),
+								ifDev(`webpack-hot-middleware/client?http://localhost:${env.port}`),
+								path.join(__dirname, '../src/index.js')
+						]),
+						vendor: ['react', 'react-dom'],
+				},
+
+				resolve: {
+						/*
+						 * An array of extensions that should be used to resolve modules.
+						 *
+						 * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+						 */
+						extensions: ['.js', '.json'],
+				},
+				output: {
+						filename: '[name].[hash].js',
+						sourceMapFilename: '[name].[hash].map.js',
+						path: path.join(__dirname, '../build/'),
+						// publicPath: '/', can uncomment if you want everything relative to root '/'
+				},
+
+				module: {
+						loaders: [
+								{
+										test: /\.js?$/,
+										exclude: /node_modules/,
+										loader: ['react-hot-loader/webpack',
+										         'babel-loader'],
+								},
+								{
+										test: /\.(css)$/,
+										loaders:
+										      env.dev || env.test
+												      ? [
+														      'style-loader',
+														      'css-loader?modules=true&minimize&-autoprefixer',
+														      'postcss-loader'
+												      ]
+												      : ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules=true&minimize&-autoprefixer!postcss-loader' }),
+								},
+								{
+										test: /\.(png|jpg)$/,
+										loader: 'url-loader?limit=8192'
+								}
+						],
+				},
+
+				plugins: removeEmpty([
+						new webpack.optimize.CommonsChunkPlugin({
+								name: 'vendor',
+								minChunks: Infinity,
+								filename: '[name].[hash].js',
+						}),
+						new HtmlWebpackPlugin({
+								template: path.join(__dirname, '../src/frontend/index.html'),
+								filename: 'index.html',
+								inject: 'body',
+						}),
+						new webpack.LoaderOptionsPlugin({
+								minimize: env.prod,
+								debug: env.dev,
+								options: {
+										context: __dirname,
+										postcss: [Autoprefixer({ browsers: ['last 3 versions'] })],
+								},
+						}),
+						new webpack.HotModuleReplacementPlugin(),
+						new webpack.NamedModulesPlugin(),
+
+						new webpack.DefinePlugin({
+								__DEVELOPMENT__: Boolean(env.dev),
+								'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+						}),
+
+						ifProd(new ExtractTextPlugin({
+								filename: '[name].[hash].css',
+						})),
+						ifProd(new webpack.optimize.UglifyJsPlugin({
+								compress: {
+										'screw_ie8': true,
+										'warnings': false,
+										'unused': true,
+										'dead_code': true,
+								},
+								output: {
+										comments: false,
+								},
+								sourceMap: false,
+						})),
+				]),
+		};
+};
